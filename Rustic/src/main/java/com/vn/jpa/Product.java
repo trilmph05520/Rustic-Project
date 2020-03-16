0082b7826c0da18386950635e3b6114a00424695
trilmph05502 <49575866+trilmph05520@users.noreply.github.com>
2020-03-16 08:31:30 +0700

    ccm fix created date

--- Rustic/src/main/java/com/vn/jpa/Product.java
+++ Rustic/src/main/java/com/vn/jpa/Product.java
@@ -1,10 +1,21 @@
 package com.vn.jpa;
 
-import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "product")
@@ -204,7 +215,11 @@ public class Product implements Serializable {
     @PrePersist
     public void prePersist(){
         if(this.createDate == null){
-            this.createDate = new Date();
+        	Date create = new Date();
+        	create.setHours(0);
+        	create.setMinutes(0);
+        	create.setSeconds(0);
+            this.createDate = create;
         }
     }
 }
