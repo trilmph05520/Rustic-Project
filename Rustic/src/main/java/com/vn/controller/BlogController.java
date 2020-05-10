85a397e5fc77e7a26d3c3ea569e8d52697050943
longnsph05386 <40315550+longnsph05386@users.noreply.github.com>
2020-05-10 11:34:06 +0700

    cm blog

--- Rustic/src/main/java/com/vn/controller/BlogController.java
+++ Rustic/src/main/java/com/vn/controller/BlogController.java
@@ -1,12 +1,15 @@
 package com.vn.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -17,7 +20,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +39,7 @@ import com.vn.jpa.Blog;
 import com.vn.jpa.Category;
 import com.vn.jpa.Product;
 import com.vn.model.BlogModel;
+import com.vn.model.CategoryModel;
 import com.vn.model.ProductModel;
 import com.vn.service.BlogService;
 
@@ -98,11 +105,118 @@ public class BlogController {
 			blog.setTitle(model.getTitle());
 			blogService.insert(blog);
 			
-			responseMap.put("success", "Thêm sản phẩm thành công");
+			responseMap.put("success", "Thêm bài viết thành công");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return gson.toJson(responseMap);
+	}
+	
+	
+	@RequestMapping(value = "{id}/update.html", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
+	public String update(Model model, @PathVariable("id") Long id,
+			@ModelAttribute("product") @Valid ProductModel productModel, BindingResult result) {
+		Blog blog = blogService.findOne(id);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		model.addAttribute("blog", blog);
+		model.addAttribute("subImg", gson.toJson(blog.getSubImg()));
+		return "admin/blog/update";
+	}
+	
+	@RequestMapping(value = "{id}/update.html", method = RequestMethod.POST)
+	@PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
+	public @ResponseBody String updateBlog(@PathVariable("id") Long id, @RequestBody(required = false) BlogModel model) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		Map<String, Object> responseMap = new HashMap<>();
+		Blog blog = blogService.findOne(id);
+		String filePathMain = ROOT_FOLDER + MAIN_ADDRESS;
+		String sorceWebPathMain = HOST_ADDRESS + MAIN_ADDRESS;
+		String filePathSub = ROOT_FOLDER + SUB_ADDRESS;
+		String sorceWebPathSub = HOST_ADDRESS + SUB_ADDRESS;
+		try {
+			if (model.getMainImg() != null) {
+				String mainImg = blog.getMainImg();
+				if (!Strings.isNullOrEmpty(mainImg)) {
+					mainImg = FileUtils.changeWebSourcePathToFilePath(mainImg, filePathMain, sorceWebPathMain);
+					boolean deleteMain = FileUtils.deleteSingleImage(mainImg);
+					if (deleteMain) {
+						mainImg = "";
+					}
+				}
+				FileUtils.Result resultMain = FileUtils.storageFile(filePathMain, model.getMainImg(), false, false,
+						sorceWebPathMain);
+				blog.setMainImg(resultMain.getResult());
+			}
+			String oldSubImg = blog.getSubImg();
+			if (!Strings.isNullOrEmpty(model.getRemoveSub())) {
+				List<String> lsSubDelete = FileUtils.getImgPathFromString(model.getRemoveSub());
+				for (String str : lsSubDelete) {
+					if (!Strings.isNullOrEmpty(str)) {
+						boolean isDelete = false;
+						if (oldSubImg.contains("\"" + str + "\"" + ",")) {
+							oldSubImg = oldSubImg.replace("\"" + str + "\"" + ",", "");
+							isDelete = true;
+						} else if (oldSubImg.contains("," + "\"" + str + "\"") && !isDelete) {
+							oldSubImg = oldSubImg.replace("," + "\"" + str + "\"", "");
+							isDelete = true;
+						} else if (oldSubImg.contains("\"" + str + "\"") && !isDelete) {
+							oldSubImg = oldSubImg.replace("\"" + str + "\"", "");
+							isDelete = true;
+						}
+						if (isDelete) {
+							str = FileUtils.changeWebSourcePathToFilePath(str, filePathSub, sorceWebPathSub);
+							FileUtils.deleteSingleImage(str);
+						}
+					}
+				}
+			}
+			if (model.getSubImg() != null && model.getSubImg().length > 0) {
+				FileUtils.Result resultSub = FileUtils.storageFile(filePathSub, model.getSubImg(), false, false,
+						sorceWebPathSub);
+				if (resultSub.isSuccess()) {
+					String subImgs = resultSub.getResult();
+					if (Strings.isNullOrEmpty(oldSubImg)) {
+						oldSubImg = subImgs;
+					} else {
+						if (subImgs.startsWith("[")) {
+							subImgs = subImgs.substring(1);
+						}
+						if (oldSubImg.endsWith("]")) {
+							oldSubImg = oldSubImg.substring(0, oldSubImg.length() - 1);
+						}
+						if (oldSubImg.trim().length() <= 1) {
+							oldSubImg = "[" + subImgs;
+						} else {
+							oldSubImg = oldSubImg + "," + subImgs;
+						}
+					}
+				}
+			}
+			blog.setSubImg(oldSubImg);
+			blog.setMainDescription(model.getMainDescription());
+			blog.setSubDescription(model.getSubDescription());
+			blog.setTitle(model.getTitle());
+			blogService.update(blog);
+			
+			responseMap.put("success", "Sửa bài viết thành công");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return gson.toJson(responseMap);
+	}
+	
+	@RequestMapping(value = "delete/{id}/list.html")
+	@PreAuthorize("hasAnyAuthority('Administrators','Staffs')")
+	public String delete(Model model, @PathVariable("id") Long id) {
+		try {
+			Blog blog = blogService.findOne(id);
+			if(blog!=null)
+				blogService.delete(blog);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/blog/list.html";
 	}
 	
 }
